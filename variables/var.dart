dacd01199170f11f289d6771524ac06062bfb39c
B10101 <b37710101@gmail.com>
2024-04-22 23:52:51 +0300

    variables - list

--- variables/var.dart
+++ variables/var.dart
@@ -15,6 +15,48 @@ void main() {
   // dynamic
   dynamic follow = true;
 
-  print("string: $her, integer: $age, double: $mean, bool: $isAdult, dynamic: $follow");
+  print(
+      "string: $her, integer: $age, double: $mean, bool: $isAdult, dynamic: $follow");
+  print("");
 
+  // lists
+
+  //create a list
+
+  var list1 = [1, 2, 3, 4];
+  print(list1);
+  print(list1[0]);
+
+  // change an item
+
+  list1[1] = 4;
+  print(list1);
+
+  // an empty list
+  var list2 = [];
+
+  // adding to a list
+
+  list2.add(2);
+  print(list2);
+  // add multiple stuff
+  list2.addAll([1, 2, 3, 4, 5]);
+  print(list2);
+  print("");
+
+  // adding to a specific index
+  list2.insert(3, 'real');
+  print(list2);
+
+  // inserting many items
+  list2.insertAll(2, [21, 45, 65, 'fear']);
+  print(list2);
+
+  //removing from a list
+  list2.remove(45);
+  print(list2);
+
+  // removing a specific index
+  list2.removeAt(2);
+  print(list2);
 }
