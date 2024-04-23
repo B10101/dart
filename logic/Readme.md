d1cf326138e07cdce9607b73bb17c7edcfe877c0
B10101 <b37710101@gmail.com>
2024-04-23 09:17:28 +0300

    logic

---
+++ logic/Readme.md
@@ -0,0 +1,2 @@
+# Logic
+there are 3 trypes if, if else, if else if
---
+++ logic/logic.dart
@@ -0,0 +1,18 @@
+void main() {
+  var num = 4;
+
+  if (num == 3) {
+    print("the number is 3");
+  } else {
+    print("the number is not 3");
+  }
+
+  var age = 23;
+  if (age < 18) {
+    print("minor");
+  } else if (age <= 60) {
+    print("working adult");
+  } else {
+    print("Aged");
+  }
+}
